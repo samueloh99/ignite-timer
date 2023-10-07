8ec774134dddf7a196fe6e6f501726edc0ff653d
samueloh99 <samueloh99@gmail.com>
2023-10-07 12:17:57 -0300

    Added react-router-dom and created files for path (home and history)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
+import { BrowserRouter } from "react-router-dom";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="secondary" />
-
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
