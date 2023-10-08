8cfd1a59fec11ddd032a0518889f9f441bd8dbfb
samueloh99 <samueloh99@gmail.com>
2023-10-08 06:03:00 -0300

    Created useState for new cycle on homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Play } from "phosphor-react";
 
 import { useForm } from "react-hook-form";
@@ -26,7 +28,15 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
